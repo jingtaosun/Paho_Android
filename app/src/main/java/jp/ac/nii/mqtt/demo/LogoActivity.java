96cb429aaf6363f11f34bfe05039b89526ed0f71
Sun <sun@nii.ac.jp>
2019-09-08 22:21:14 +0900

    new design for MQTT demo

---
+++ app/src/main/java/jp/ac/nii/mqtt/demo/LogoActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.nii.mqtt.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
